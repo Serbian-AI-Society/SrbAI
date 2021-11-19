ad854581935d4a743af2aaed6aa0d446434926e3
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2021-11-19 22:02:19 +0100

    Part of Speech tagging added

--- tests/sintaktickiOp_testovi.py
+++ tests/sintaktickiOp_testovi.py
@@ -1,5 +1,7 @@
+import os
 import unittest
 from srbai.SintaktickiOperatori.stemmer_nm import stem_str, stem_arr
+from srbai.SintaktickiOperatori.POS_tagger import POS_Tagger
 
 
 class SintaktickiOperatoriTestovi(unittest.TestCase):
@@ -10,6 +12,14 @@ class SintaktickiOperatoriTestovi(unittest.TestCase):
         expect = ['jovi', 'jesam', 'isx', 'u', 'sxkol', '.', 'marij', 'jesam', 'dobr', 'devoj', '.']
         self.assertEqual(sent2, expect)
 
-
+    def test_tagger(self):
+        pt = POS_Tagger()
+        tags = pt.tag('Jovica je išao u školu. Marija je dobra devojka.')
+        self.assertEqual(tags,[('Jovica', b'N-msn'), ('je', b'Vcr3s'), ('išao', b'Vmp-sm'), ('u', b'Sa'), ('školu', b'N-fsa'), ('.', b'Z'), ('Marija', b'N-fsn'), ('je', b'Vcr3s'), ('dobra', b'Agpfsn'), ('devojka', b'N-fsn'), ('.', b'Z')])
+        tags = pt.tag('Predlozi su nepromenljive vrste reči koje izražavaju odnos između bića, stvari i pojava i utiču na padež reči uz koju stoje.  Stoje uz imenice i zamenice.')
+        print(tags)
+        tags = pt.tag(
+            'Radn površina')
+        print(tags)
 if __name__ == '__main__':
     unittest.main()
