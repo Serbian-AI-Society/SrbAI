c81365e9092ad2943780d5e3e8fb53c40091819a
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2021-11-24 20:46:21 +0100

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="SrbAI",
-    version="0.0.1",
+    version="0.0.2",
     author="Serbian AI Society",
     author_email="nikola.milosevic86@gmail.com",
     description="Library for processing serbian language",
