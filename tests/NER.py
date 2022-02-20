3437b9ebf0d7766d2211679c08631728c0555487
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2022-02-20 19:43:01 +0100

    Added test for NER and dependency parsing

---
+++ tests/NER.py
@@ -0,0 +1,14 @@
+import unittest
+from srbai.NER.NER_classla import NER_classla
+
+
+class NER_Testovi(unittest.TestCase):
+    def test_classla_NER(self):
+        ner = NER_classla()
+        res = ner.perform_NER("Nikola je bio u Parizu.")
+        true_res = [{'text': 'Nikola', 'NER': 'B-PER', 'dep_rel': 'nsubj'}, {'text': 'je', 'NER': 'O', 'dep_rel': 'aux'}, {'text': 'bio', 'NER': 'O', 'dep_rel': 'root'}, {'text': 'u', 'NER': 'O', 'dep_rel': 'case'}, {'text': 'Parizu', 'NER': 'B-LOC', 'dep_rel': 'obl'}, {'text': '.', 'NER': 'O', 'dep_rel': 'punct'}]
+        self.assertEqual(res,true_res)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
