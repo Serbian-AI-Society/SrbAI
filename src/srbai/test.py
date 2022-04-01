3ca492b3522d48e48dc677595213a5ba6e93eb6d
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2022-04-01 22:24:06 +0200

    Lemmatizator wrapper implemented

--- src/srbai/test.py
+++ src/srbai/test.py
@@ -1,6 +1,18 @@
 import classla
-classla.download('sr')
-nlp =classla.Pipeline('sr')
-doc = nlp("Milica voli da ide u školu koja se nalazi u Novom Sadu.")
-print(doc.to_dict()[0][0])
 
+classla.download('sr')
+nlp = classla.Pipeline('sr', processors='tokenize,pos,lemma')
+
+def get_lemmas(text: str):
+    doc = nlp(text)
+    dc = doc.to_dict()
+    lemmas = []
+    for da in dc:
+        arr = da[0]
+        print(arr)
+        for token in arr:
+            lemmas.append(token['lemma'])
+    return lemmas
+
+lemme = get_lemmas("Milica voli da ide u školu koja se nalazi u Novom Sadu. Nije ništa rekla. ")
+print(lemme)
